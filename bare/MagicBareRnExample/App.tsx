a859ce845ead8583033376e7b171c32d45835395
Arian Flores <arian.flores@magic.link>
2023-01-06 13:10:41 -0800

    Reorg files under bare and expo

--- bare/MagicBareRnExample/App.tsx
+++ bare/MagicBareRnExample/App.tsx
@@ -17,7 +17,6 @@ export default function App() {
 
   const magic = new Magic(API_KEY[env], {
     extensions: [new OAuthExtension()],
-    locale: "pl_en"
   });
 
   const web3 = new Web3(magic.rpcProvider);
