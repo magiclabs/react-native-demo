25de9844d582d7abb465e42545bc048e0a642c67
Arian Flores <arian.flores@magic.link>
2023-01-11 15:06:28 -0800

    More Clean up

--- bare/MagicLegacyBareRnExample/App.tsx
+++ bare/MagicLegacyBareRnExample/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BottomTabNavigator } from './navigation';
 import { Magic } from '@magic-sdk/react-native'
 import Web3 from 'web3'
-import { ENV, API_KEY, MGBOX_ENDPOINT } from './config/env';
+import { ENV, API_KEY } from './config/env';
 import {SafeAreaView} from "react-native";
 import {NavigationContainer} from "@react-navigation/native";
 
