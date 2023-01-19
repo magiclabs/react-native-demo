f2d690fae543890cc374b05b30baba4be0ee9145
Arian Flores <arian.flores@magic.link>
2023-01-19 15:07:24 -0800

    Adds Magic Connect to Bare Sample + Updats RN to latest

--- bare/MagicBareRnExample/App.tsx
+++ bare/MagicBareRnExample/App.tsx
@@ -5,6 +5,7 @@ import Navigation from './navigation';
 
 import { Magic } from '@magic-sdk/react-native-bare'
 import { OAuthExtension } from "@magic-ext/react-native-bare-oauth";
+import { ConnectExtension } from '@magic-ext/connect';
 import Web3 from 'web3'
 import { ENV, API_KEY } from './config/env';
 
@@ -14,7 +15,7 @@ export default function App() {
   const [env, setEnv] = React.useState(ENV.PROD);
 
   const magic = new Magic(API_KEY[env], {
-    extensions: [new OAuthExtension()],
+    extensions: [new OAuthExtension(), new ConnectExtension()],
   });
 
   const web3 = new Web3(magic.rpcProvider);
