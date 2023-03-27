288a2bc286b1628ca8541ba788740d853eb34281
Arian Flores <arian.flores@magic.link>
2023-03-27 14:55:58 -0700

    Remove Legacy Packages

--- expo/MagicLegacyExpoRNExample/metro.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    resolver: {
-
-        /* Step 1: Add extra Node Modules to support `crypto` package for web3.js */
-        extraNodeModules: require('node-libs-browser')
-    }
-};
